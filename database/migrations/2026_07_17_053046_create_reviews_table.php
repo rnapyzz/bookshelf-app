bdf0a1599c8e39aac566cdf8f3dd8a654eaf85f3
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-07-26 14:31:50 +0900

    fix: 1つの書籍に複数回のレビューを投稿できるように変更 #64

--- database/migrations/2026_07_17_053046_create_reviews_table.php
+++ database/migrations/2026_07_17_053046_create_reviews_table.php
@@ -18,8 +18,6 @@ return new class extends Migration
             $table->tinyInteger('rating');
             $table->text('comment');
             $table->timestamps();
-
-            $table->unique(['user_id', 'book_id']);
         });
     }
 
