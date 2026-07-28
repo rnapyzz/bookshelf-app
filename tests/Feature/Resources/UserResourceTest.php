dead2c3919e8744530f4db28fe62e6d8fb970a0b
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-07-28 15:04:46 +0900

    feat: UserResourceのテストを実装 #14

---
+++ tests/Feature/Resources/UserResourceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Resources;
+
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class UserResourceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function 正しいResource形式に変換される(): void
+    {
+        $user = User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $request = Request::create('/api/v1/books/1', 'GET');
+        $resource = new UserResource($user)->toArray($request);
+
+        $this->assertEquals([
+            'name' => 'Test User',
+        ], $resource);
+    }
+}
