568433623ece1ba251c94bd945a11f54ac89d185
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-07-28 14:50:28 +0900

    feat: ReviewResourceのテストを実装 #14

---
+++ tests/Feature/Resources/ReviewResourceTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature\Resources;
+
+use App\Http\Resources\ReviewResource;
+use App\Models\Review;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Request;
+use Tests\TestCase;
+
+class ReviewResourceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function 正しいResource形式に変換される(): void
+    {
+        $user = User::factory()->create([
+            'name' => 'Testman',
+            'email' => 'test@example.com',
+        ]);
+
+        $review = Review::factory()->create([
+            'user_id' => $user->id,
+            'rating' => 5,
+            'comment' => 'Perfect!!',
+        ]);
+
+        $request = Request::create('/api/v1/books'.$review->book_id, 'GET');
+        $resource = new ReviewResource($review)->toArray($request);
+
+        $this->assertEquals($review->id, $resource['id']);
+        $this->assertEquals(5, $resource['rating']);
+        $this->assertEquals('Perfect!!', $resource['comment']);
+        $this->assertEquals($review->created_at, $resource['created_at']);
+        $this->assertEquals($review->updated_at, $resource['updated_at']);
+    }
+}
